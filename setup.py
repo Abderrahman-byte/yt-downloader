f654803f4a9f7282e480f591f4ac8a3cfcef45da
abderahman-byte <el.abderrahman00@gmail.com>
2020-05-10 12:58:52 +0000

    finished

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='downloader',
     version="0.1.0",
     packages=["downloader"],
+    auhtor="abderrahman elasri",
     entry_points = {
         'console_scripts': [
             'downloader = downloader.__main__:main'
